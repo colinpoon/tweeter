80d60e1ea0bc81687a1ca1552b236590ec4f7ae0
Colin Poon <me@colinpoon.com>
2021-09-08 07:12:32 +0000

    liquide resizing for header, main, and new-tweet

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,22 @@
-const div1 = document.getElementById("counter");
+
 $(document).ready(function() {
-console.log(this);
+  const tweetText = document.getElementById("tweet-text");
+  console.log(this);
+
+$(tweetText).on('input', function(event) {
+  // console.log(event.target.value);
+  let text = event.target.value;
+  let output = $("output.counter")
+  if (text.length < 0) {
+    // add danger text class
+    
+  } else {
+    // remove danger text class (140 -text.length)
+  }
+
 });
 
-$("#btn").on('click', function() {
-  console.log(this); //The this keyword is a reference to the button
+
 });
 
-$("#btn").on('click', () => {
-  console.log(this); //The this keyword here refers to something else!
-});
+//jq selector method
