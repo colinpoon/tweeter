09ddec9fd287fce1fe894475d51f18c0e63b963c
Colin Poon <me@colinpoon.com>
2021-10-05 13:20:22 -0700

    character count implmented & functional

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,79 +1,18 @@
 
-
-
-/*
-> set characterCount as a variable equal to 140 
-> On keypress or key down character count -1 
-> On delete (key id of delete) character count +1
-> If character count < 0 change color (and make it negative if needed)
-> If character count < 0 return an error message... (event?)
->if character count > 0 then tweet can be sent.
-*/
-$(document).ready(function() {
-  // console.log("I'm ready! <----------");
-  // console.log(tweetText);
- const tweetText = document.getElementById("tweet-text");
- const counter = document.getElementById("counter");
-
-$( "#tweet-text" ).keypress(function() {
-  console.log( "Handler for .keypress() called." );
-  // console.log('Keypress event', this);
+// CHARACTER COUNTER
+$(document).ready(function () {
+  const tweetText = document.getElementById("tweet-text");
+  // EVENTS TRIGGERS ON KEY UP
+  $(tweetText).on('keyup', function (event) {
+    let charCount = event.target.value.length;
+    const $counter = $(this).siblings('.submit').children('.counter');
+    $counter.text(140 - charCount);
+    // CSS CHANGE TO RED WHEN COUNTER < 0
+    if (charCount > 140) {
+      $counter.css('color', '#f97f58');
+    } else {
+      // CSS CHANGE TO DEFAULT WHEN COUNTER RETURNS BACK TO > 0
+      $counter.css('color', '#204d36')
+    }
+  });
 });
-$( "#tweet-text" ).keyup(function() {
-  console.log( "Handler for .keyUP() called." );
-  // console.log('Key Up event', this);
-});
-// $( "#counter" ).click(function() {
-//   $( "#tweet-text" ).keypress();
-//   $( "#tweet-text" ).keypress(this);
-// });
-$("#tweet-text").on('click', function() {
-  console.log(this);
-});
-});
-
-
-
-
-
-
-
-
-// $(document).ready(function() {
-//   const tweetText = document.getElementById("tweet-text");
-//   console.log(this);
-
-// $(tweetText).on('input', function(event) {
-//   // console.log(event.target.value);
-//   let text = event.target.value;
-//   let output = $("output.counter")
-//   if (text.length < 0) {
-//     // add danger text class
-    
-//   } else {
-//     // remove danger text class (140 -text.length)
-//   }
-
-// });
-
-
-// });
-
-// //jq selector method
-
-// $( "#tweet-text" ).keydown(function() {
-//   // console.log( "Handler for .keypress() called." );
-//   console.log('Key Down event', this);
-// });
-// $( "#tweet-text" ).blur(function() {
-//   // console.log( "Handler for .keypress() called." );
-//   console.log('Blur event', this);
-// });
-// $( "#tweet-text" ).change(function() {
-//   // console.log( "Handler for .keypress() called." );
-//   console.log('Change event', this);
-// });
-// $( "#tweet-text" ).input(function() {
-//   // console.log( "Handler for .keypress() called." );
-//   console.log('Input event', this);
-// });
