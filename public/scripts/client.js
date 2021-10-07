86e05cd50b13541665675eefb2913cb78c8586ae
Colin Poon <me@colinpoon.com>
2021-10-06 22:02:06 -0700

    add loadpost function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,33 +1,33 @@
-// TEST 
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1633374227419
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://github.com/colinpoon/tweeter/blob/master/public/images/tweeter1.png?raw=true",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1633374227419 
-  }
-];
+// // TEST 
+// const tweetData = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1633374227419
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://github.com/colinpoon/tweeter/blob/master/public/images/tweeter1.png?raw=true",
+//       "handle": "@rd"
+//     },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1633374227419
+//   }
+// ];
 
 
 $(document).ready(function () {
-const createTweetElement = function (tweet) {
-  let $tweet = `
+  const createTweetElement = function (tweet) {
+    let $tweet = `
   <article class="tweets">
     <header class="tweet-header">
       <span class="tweeter-profile">
@@ -54,21 +54,84 @@ const createTweetElement = function (tweet) {
         </div>
       </footer>
   </article>`;
-  return $tweet;
-};
+    return $tweet;
+  };
 
-const renderTweets = function (tweets) {
-  tweets.forEach(element => {
-    // calls createTweetElement for each tweet
-    const tweet = createTweetElement(element);
-    // takes return value and appends it to the tweets container 
-    $('#tweets-container').append(tweet);
+  const renderTweets = function (tweets) {
+    tweets.forEach(element => {
+      // calls createTweetElement for each tweet
+      const tweet = createTweetElement(element);
+      // takes return value and appends it to the tweets container 
+      $('#tweets-container').prepend(tweet);
+    });
+  };
+
+  $("#tweet-form").submit(function (event) {
+    event.preventDefault();
+    const tweetData = $("#tweet-text").serialize();
+    $.ajax({
+      type: "POST",
+      url: "/tweets/",
+      data: tweetData,
+    })
+    .then(function(data){
+     loadTweets();
+    })
   });
-};
-renderTweets(tweetData);
 
+  const loadTweets = function () {
+    const $tweetData = $('#tweets-container');
+    const $tweetText = $('#tweet-text');
+    $.ajax({ 
+      url: '/tweets/',
+      method: 'GET',
+      dataType: "JSON",
+    })
+    .then(function(data){
+      $tweetData.empty();
+      renderTweets(data) 
+    })
+  };
+  loadTweets();
 });
 
+// $("#tweet-form").submit(function (event) {
+//   // $("#tweet-form").on("submit", function(event) {
+//     event.preventDefault();
+//     const text = $("tweet-text").val();
+//     console.log(text);
+//     // if (count === 0) {
+//     //   return alert('Nothing to tweet!');
+//     // }
+//     // if (count > 140){
+//     //   return alert('You talk too much!');
+//     // }
+//     const tweetData = $("#tweet-text").serialize();
+//     $.ajax({
+//       type: "POST",
+//       url: "/tweets/",
+//       data: tweetData,
+//     })
+//     .then(function(data){
+//      loadTweets();
+//     })
+  // });
+
+/// .DONE & .FAIL METHOD
+
+// function getData() {
+//   return $.ajax({
+//       url : 'example.com',
+//       type: 'GET'
+//   });
+// }
+
+// function handleData(data /* , textStatus, jqXHR */ ) {
+//   alert(data);
+//   //do some stuff
+// }
+
+// getData().done(handleData);
 
 
 
