07eb5ce6eb4115c0278701d59e6fe633867f9a2e
Colin Poon <me@colinpoon.com>
2021-10-07 18:59:30 -0700

    Back to top stretch

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -89,4 +89,21 @@ $(document).ready(function () {
       })
   };
   loadTweets();
+
+  $(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() - 200 > 0) {
+        $('#to-top').stop().slideDown('fast');
+      } else {
+        $('#to-top').stop().slideUp('fast'); 
+      }
+      $("#to-top").on("click", function () {
+        $("html, body").animate({
+          scrollTop: 0
+        }, 200);
+      });
+    });
+  });
+
 });
+
